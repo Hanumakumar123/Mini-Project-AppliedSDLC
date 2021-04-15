1e7c68c8a7b778de211fff7264c8de14d85dc42e
Hanumakumar123 <62949244+Hanumakumar123@users.noreply.github.com>
2021-04-16 00:35:52 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,112 +1,96 @@
-#include <calculator_operations.h>
+#include<stdio.h>
 
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
+#define CANDIDATE_COUNT
 
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
+#define CANDIDATE1 "David Hull"
+#define CANDIDATE2 "Kristin Canella"
+#define CANDIDATE3 "Jim Brar"
+#define CANDIDATE4 "Donald Truimph"
 
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
+int votesCount1=0, votesCount2=0, votesCount3=0, votesCount4=0, spoiledtvotes=0;
 
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+void castVote()
 {
-    printf("\n****Calculator****\n");
-    while(1)
-    {
-        calculator_menu();
-    }
+int choice; 
+int age;
+printf("\n Enter the age of the persson");   
+printf("\n\n ### Please choose your Candidate ####\n\n");
+printf("\n 1. %s", CANDIDATE1);
+printf("\n 2. %s", CANDIDATE2);
+printf("\n 3. %s", CANDIDATE3);
+printf("\n 4. %s", CANDIDATE4);
+printf("\n 5. %s", "None of These");
+
+printf("\n\n Input your choice (1 - 4) : ");
+scanf("%d",&choice);
+scanf("%d",&age);
+
+switch(choice){
+    case 1: votesCount1++; break;
+    case 2: votesCount2++; break;
+    case 3: votesCount3++; break;
+    case 4: votesCount4++; break;
+    case 5: spoiledtvotes++; break;
+    default: printf("\n Error: Wrong Choice !! Please retry");
+             //hold the screen
+             getchar();
+}
+printf("\n thanks for vote !!");
 }
 
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     // __fpurge(stdin);
-    scanf("%d", &calculator_operation);
+void votesCount(){
+printf("\n\n ##### Voting Statics ####");
+printf("\n %s - %d ", CANDIDATE1, votesCount1);
+printf("\n %s - %d ", CANDIDATE1, votesCount2);
+printf("\n %s - %d ", CANDIDATE1, votesCount3);
+printf("\n %s - %d ", CANDIDATE1, votesCount4);
+printf("\n %s - %d ", "Spoiled Votes", spoiledtvotes); 
+}
 
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        // __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
+void getLeadingCandidate(){
+    printf("\n\n  #### Leading Candiate ####\n\n");
+    if(votesCount1>votesCount2 && votesCount1>votesCount3 && votesCount1 >votesCount4)
+    printf("[%s]",CANDIDATE1);
+    else if (votesCount2>votesCount3 && votesCount2>votesCount4 && votesCount2 >votesCount1)
+    printf("[%s]",CANDIDATE2);
+    else if(votesCount3>votesCount4 && votesCount3>votesCount2 && votesCount3 >votesCount1)
+    printf("[%s]",CANDIDATE3);
+    else if(votesCount4>votesCount1 && votesCount4>votesCount2 && votesCount4 >votesCount3)
+    printf("[%s]",CANDIDATE4);
     else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        // __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            // __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
+    printf("----- Warning !!! No-win situation----");    
+    
+    
+    
 }
 
-int valid_operation(int operation)
+int main()
 {
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+int i;
+int choice;
+
+do{
+printf("\n enter age of person");
+printf("\n\n ###### Welcome to Election/Voting 2019 #####");
+printf("\n\n 1. Cast the Vote");
+printf("\n 2. Find Vote Count");
+printf("\n 3. Find leading Candidate");
+printf("\n 0. Exit");
+
+printf("\n\n Please enter your choice : ");
+scanf("%d", &choice);
+
+switch(choice)
+{
+case 1: castVote();break;
+case 2: votesCount();break;
+case 3: getLeadingCandidate();break;
+default: printf("\n Error: Invalid Choice");
+}
+}while(choice!=0);
+
+//hold the screen
+getchar();
+
+return 0;
 }
