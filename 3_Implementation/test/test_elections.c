6e9f5f64252a92b024e705de63ddcd8bbb0fa9cc
Hanumakumar123 <62949244+Hanumakumar123@users.noreply.github.com>
2021-04-16 15:12:31 +0530

    Update test_elections.c

--- 3_Implementation/test/test_elections.c
+++ 3_Implementation/test/test_elections.c
@@ -1,1 +1,38 @@
+#include "unity.h"
+
+/* Modify these two lines according to the project */
+#include "Elections.h"
+#define PROJECT_NAME   "Election voteing"
+
+/* Prototypes for all the test functions */
+void test_voidvotescount(void);
+
+
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_votescount);
+  
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_votescount(void) {
+  TEST_ASSERT_EQUAL(6,votescount());
+  TEST_ASSERT_EQUAL(7,votescount());
+  
+}
 
